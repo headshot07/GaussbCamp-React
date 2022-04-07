bf45fa5c4d3f8f9c90aac4c50a3a6e6ec34fa095
Sanjay Singh <sanjaythakur1729@gmail.com>
2022-04-07 10:39:03 +0530

    Fixed Authentication Bug

--- src/Routes/protectedRoutes.js
+++ src/Routes/protectedRoutes.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Navigate } from 'react-router-dom';
-import AuthLogin from "../Services/authLogin";
+import AuthLogin from '../Services/authLogin';
 
 const ProtectedRoutes = ({ component: Component, ...rest }) => {
-    let isLoggedIn = AuthLogin();
+    let isLoggedIn = AuthLogin()
     return isLoggedIn ? Component : <Navigate to="/" />;
 }
 
--- src/Services/authLogin.js
+++ src/Services/authLogin.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-function AuthLogin(){
+export default function AuthLogin(){
     const jwtToken = localStorage.getItem('jwtToken');
-    if(jwtToken === 'null') return false
+    console.log(jwtToken)
+    if(jwtToken === null || jwtToken === 'null') return false
     return true
 }
-
-export default AuthLogin
 
